2b324b95ee2c58faf278f386a414451b18503d58
Bill-Flash <bill6176@connect.hku.hk>
2025-09-17 14:33:38 +0800

    gen3d and recursive version for xyz to sdf

--- run_obabel.py
+++ run_obabel.py
@@ -2,20 +2,22 @@ import os
 import sys
 import subprocess
 
-
 def convert(in_path, out_path):
     print(f'Converting {in_path} to {out_path}')
-    subprocess.run(f'obabel {in_path} -O {out_path} --gen3d', shell=True)
-
+    subprocess.run(f'obabel "{in_path}" -O "{out_path}" --gen3d', shell=True)
 
 input_dir = sys.argv[1]
 output_dir = sys.argv[2]
 os.makedirs(output_dir, exist_ok=True)
 
-for in_fname in os.listdir(input_dir):
-    print(in_fname)
-    if in_fname.endswith('.xyz'):
-        out_fname = in_fname.replace('.xyz', '.sdf')
-        input_path = os.path.join(input_dir, in_fname)
-        output_path = os.path.join(output_dir, out_fname)
-        convert(input_path, output_path)
+for root, dirs, files in os.walk(input_dir):
+    rel_dir = os.path.relpath(root, input_dir)  # 子目录名
+    out_subdir = os.path.join(output_dir, rel_dir)
+    os.makedirs(out_subdir, exist_ok=True)
+
+    for f in files:
+        if f.endswith('.xyz'):
+            in_path = os.path.join(root, f)
+            out_fname = f.replace('.xyz', '.sdf')
+            out_path = os.path.join(out_subdir, out_fname)
+            convert(in_path, out_path)
